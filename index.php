5cd0ee9b4c4fc35cb172d1b96855a5d162cab054
Houston <willygames56@gmail.com>
2022-04-05 17:21:27 +0200

    remplacement aboslu to normal in index

--- index.php
+++ index.php
@@ -19,9 +19,10 @@ $connexion->exec($reqSQL);
 <html lang="fr">
 
 <head>
-    <?php
-    include("/includes/head.inc.php");
-    ?>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="http://gsb.wilfriedlefebvre.fr/assets/css/css.css">
+    <script type="text/javascript" language="JavaScript" src="http://gsb.wilfriedlefebvre.fr/assets/js/js.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Se connecter - GSB</title>
 </head>
 
