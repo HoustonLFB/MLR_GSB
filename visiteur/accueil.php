d64c11da7e93d49fa050713dac84b8661a810498
Houston <willygames56@gmail.com>
2022-04-05 17:10:30 +0200

    head total accueil visiteur
    à changer par include plus tard

--- visiteur/accueil.php
+++ visiteur/accueil.php
@@ -12,10 +12,11 @@ if ($_SESSION['logged'] && $_SESSION['NIVuser'] == 1) {
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="/assets/css/css.css">
+        <script type="text/javascript" language="JavaScript" src="/assets/js/js.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Accueil visiteur - GSB</title>
-        <link rel="stylesheet" href="../assets/css/css.css">
-		<meta charset=utf-8>
-
     </head>
     <body>
         <div id="Accueil">
