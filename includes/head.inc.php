75be044ae07a4b9858ba1a870d29397176b1d520
Houston <willygames56@gmail.com>
2022-04-06 17:54:58 +0200

    head w/ new dn

--- includes/head.inc.php
+++ includes/head.inc.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="http://gsb.wilfriedlefebvre.fr/assets/css/css.css">
-<script type="text/javascript" language="JavaScript" src="http://gsb.wilfriedlefebvre.fr/assets/js/js.js"></script>
+<link rel="stylesheet" type="text/css" href="http://mlr-gsb.site/assets/css/css.css">
+<script type="text/javascript" language="JavaScript" src="http://mlr-gsb.site/assets/js/js.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="icon" href="http://gsb.wilfriedlefebvre.fr/assets/img/favicon.ico"/>
+<link rel="icon" href="http://mlr-gsb.site/assets/img/favicon.ico"/>
