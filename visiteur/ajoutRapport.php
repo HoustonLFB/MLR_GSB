f866f0f4c5937b01929aff059f28e471ac24a169
Houston <willygames56@gmail.com>
2022-04-08 21:41:09 +0200

    CREATION FORM ET EXEC RAPPORT

--- visiteur/ajoutRapport.php
+++ visiteur/ajoutRapport.php
@@ -1,8 +1,21 @@
 <?php
 session_start();
 
+include("../includes/connexionbdd.inc.php");
+
 //RECUP INFO
 $IDuser = $_SESSION['IDuser'];
-$praID = 
+$praID = $_POST['praticienChoix'];
+$dateVisite = $_POST['dateVisite'];
+$rapBilan = $_POST['rapBilan'];
+$rapMotif = $_POST['motif'];
+$medoc1 = $_POST['medoc1'];
+$medoc2 = $_POST['medoc2'];
+$coefConf = $_POST['coefConf'];
 
-$reqSQLinto = "INSERT INTO rapportVisite VALUES (NULL, $IDuser, $praID);"
+//CREATION de la requete
+$reqSQLinto = "INSERT INTO rapportVisite VALUES (NULL, '$IDuser', $praID, now(), '$dateVisite', '$rapBilan', '$rapMotif', '$medoc1', '$medoc2', $coefConf);";
+
+$connexion->exec($reqSQLinto);
+
+header('Location: rapportForm.php');
---
+++ visiteur/rapportForm.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+
+include("../includes/connexionbdd.inc.php");
+
+
+//REQUETE ET EXECUTION
+$reqSQLpraticien = "SELECT praID, praNom, praPrenom FROM praticiens";
+$resDBpraticien = $connexion->query($reqSQLpraticien);
+
+$reqSQLmedicament = "SELECT medDepotLegal, medNomcommercial FROM medicaments";
+$resDBmedicament = $connexion->prepare($reqSQLmedicament);
+
+//RECUP ALL MEDOCS
+$medicamentsAll = $resDBmedicament->fetchAll();
+?>
+
+<!DOCTYPE HTML>
+
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="http://mlr-gsb.site/assets/css/css.css">
+    <script type="text/javascript" language="JavaScript" src="http://mlr-gsb.site/assets/js/js.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="http://mlr-gsb.site/assets/img/favicon.ico" />
+</head>
+
+<body>
+
+    <form action="ajoutRapport.php" method="POST">
+
+        Quel Praticien : <select name="praticienChoix" id="praticienChoix">
+            <?php
+            foreach ($resDBpraticien as $occurencePra) {
+                echo "<option value='" . $occurencePra['praID'] . "''>" . $occurencePra['praNom'] . " " . $occurencePra['praPrenom'] . "</option>";
+            }
+            ?>
+        </select><br>
+
+        Quand visite ? <input type="date" name="dateVisite" id="dateVisite" required><br>
+
+
+
+        Motif de visite :
+        <select name="motif" id="motif" required>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select> <br>
+
+        Medicament 1 : <select name="medoc1" id="medoc1" required>
+            <?php
+            foreach ($medicamentsAll as $occurenceMedoc) {
+                $IDmedoc = $occurenceMedoc[0];
+                $libMedoc = $occurenceMedoc[1];
+                echo "<option value='$IDmedoc'>$libMedoc</option>";
+            }
+            ?>
+        </select>
+
+        Medicament 2 : <select name="medoc2" id="medoc2">
+            <?php
+            foreach ($medicamentsAll as $occurenceMedoc) {
+                $IDmedoc = $occurenceMedoc[0];
+                $libMedoc = $occurenceMedoc[1];
+                echo "<option value='$IDmedoc'>$libMedoc</option>";
+            }
+            ?>
+        </select><br>
+
+        Coefficient de confiance :
+        1 <input type="radio" name="coefConf" id="coefConf" value=1 checked>
+        2 <input type="radio" name="coefConf" id="coefConf" value=2>
+        3 <input type="radio" name="coefConf" id="coefConf" value=3>
+        4 <input type="radio" name="coefConf" id="coefConf" value=4>
+        5 <input type="radio" name="coefConf" id="coefConf" value=5>
+
+        <br>
+        Bilan du rapport : <br>
+        <textarea name="rapBilan" id="rapBilan"></textarea><br>
+        <input type="submit" />
+
+    </form>
+
+</body>
+
+</html>
