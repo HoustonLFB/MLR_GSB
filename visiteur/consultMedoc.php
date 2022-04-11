78d9f35cf6407aab44fccfe08f03600a1386db4d
Houston <willygames56@gmail.com>
2022-04-11 20:03:21 +0200

    Create consultMedoc.php

---
+++ visiteur/consultMedoc.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+
+include("../includes/connexionbdd.inc.php");
+include("../includes/historique.inc.php");
+//include("../includes/verifConnexVisit.inc.php");
+
+$reqSQLconsultMedoc = "SELECT * FROM medicaments";
+$resBDmedoc = $connexion->query($reqSQLconsultMedoc);
+$medocAll = $resBDmedoc->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="http://mlr-gsb.site/assets/css/css.css">
+    <script type="text/javascript" language="JavaScript" src="http://mlr-gsb.site/assets/js/js.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="http://mlr-gsb.site/assets/img/favicon.ico" />
+
+    <style>
+        #tableConsultMedoc {
+            border: 1px solid #333;
+            text-align: center;
+            margin-top: 2%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+
+</head>
+
+<body>
+    <header>
+        <div class="center">
+            <img id="logo" src="../assets/img/logo-gsb.png">
+        </div>
+    </header>
+
+    <table id="tableConsultMedoc">
+        <thead>
+            <tr>
+                <th colspan="2">Consultation des Médicaments</th>
+            </tr>
+            <tr>
+                <th>Deport Légal</th>
+                <th>Nom</th>
+            </tr>
+        </thead>
+
+        <?php
+        foreach ($medocAll as $occurence) {
+            $codeMed = $occurence[0];
+            $nomMed = $occurence[1];
+
+            echo "
+                <tr>
+                    <td>$codeMed</td>
+                    <td>$nomMed</td>
+                </tr>
+            ";
+        }
+        ?>
+    </table>
+
+
+
+
+</body>
+
+</html>
