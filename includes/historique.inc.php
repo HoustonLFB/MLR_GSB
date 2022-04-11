8684fbc43faa6ffe91f32f8a66338e49f172cbce
Houston <willygames56@gmail.com>
2022-04-11 14:04:26 +0200

    ajout connexion null

--- includes/historique.inc.php
+++ includes/historique.inc.php
@@ -8,3 +8,5 @@ $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
 $reqSQLhistorique = "INSERT INTO historique VALUES ('$IP', '$url', now())";
 $connexion->exec($reqSQLhistorique);
+
+$connexion = null;
