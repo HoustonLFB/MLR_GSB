0ca93cea813196f148785d8ffa1a715b8748aad8
Houston <willygames56@gmail.com>
2022-04-11 14:07:58 +0200

    Revert "uncomment include bdd historique"
    This reverts commit f3047deba9cb936e8ef049fa637c999b42ab0123.

--- includes/historique.inc.php
+++ includes/historique.inc.php
@@ -1,7 +1,7 @@
 <?php
 //CE SCRIPT RENVOIE L'URL OU SE SITUE LE USER ET SON IP
 
-include("../includes/connexionbdd.inc.php");
+//include("../includes/connexionbdd.inc.php");
 
 $IP = $_SERVER['REMOTE_ADDR'];
 $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
