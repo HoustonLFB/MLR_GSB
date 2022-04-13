5629d0ebac51a207f8286f6da029c50e66bbc357
Houston <willygames56@gmail.com>
2022-04-13 16:58:35 +0200

    ajout titre

--- visiteur/consultMedoc.php
+++ visiteur/consultMedoc.php
@@ -21,6 +21,7 @@ $medocAll = $resBDmedoc->fetchAll();
     <script type="text/javascript" language="JavaScript" src="http://mlr-gsb.site/assets/js/js.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="http://mlr-gsb.site/assets/img/favicon.ico" />
+    <title>Consultation Médicaments - GSB</title>
 
     <style>
         #tableConsultMedoc {
--- visiteur/consultRapport.php
+++ visiteur/consultRapport.php
@@ -21,6 +21,7 @@ $rapportAll = $resBDrapport->fetchAll();
     <script type="text/javascript" language="JavaScript" src="http://mlr-gsb.site/assets/js/js.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="http://mlr-gsb.site/assets/img/favicon.ico" />
+    <title>Consultation Rapports - GSB</title>
 
     <style>
         #tableConsultRap {
