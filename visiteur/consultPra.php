cb56014aa0a7252f165ff13d85311d24507b6543
Timy_uWu <94633202+Timtim0255@users.noreply.github.com>
2022-04-14 17:51:51 +0200

    correction link css et connexionbdd.inc

--- visiteur/consultPra.php
+++ visiteur/consultPra.php
@@ -17,7 +17,7 @@ $praAll = $resBDpra->fetchAll();
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="../assets/css/css.css">
+    <link rel="stylesheet" type="text/css" href="http://mlr-gsb.site/assets/css/css.css">
     <script type="text/javascript" language="JavaScript" src="http://mlr-gsb.site/assets/js/js.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="http://mlr-gsb.site/assets/img/favicon.ico" />
