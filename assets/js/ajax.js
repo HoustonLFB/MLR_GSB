d4566749f3beff9bc7fc69764bb03522ecb20c61
Houston <willygames56@gmail.com>
2022-04-21 17:37:23 +0200

    repassage js to ajax

---
+++ assets/js/ajax.js
@@ -0,0 +1,12 @@
+// AFFICHAGE REMPLACANT BOX
+$(document).ready(function(){
+    $('#remplaDemande').on('click', function(){
+        //SI CHECKED OR NON
+       if (document.getElementById("remplaDemande").checked == true) {
+        $("#remplaDemande").after("<input type='text' name='remplaNom' id='remplaNom'>");
+       }
+       else {
+        $("#remplaNom").remove();  
+       }
+    });
+});
