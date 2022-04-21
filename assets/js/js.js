abd8547ad7c5572e8021d868f789c44e1fe918ed
Timy_uWu <94633202+Timtim0255@users.noreply.github.com>
2022-04-21 15:00:14 +0200

    Revert "ajout ajax.js dans js.js"
    This reverts commit e39996ea4a6e67739c762820afafcc579b75c2dc.

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,4 +1,3 @@
-//AFFICHAGE MSG ERREUR
 function alertMauvaiseDate() {
     alert('La date de visite ne peut pas être ultérieur à la date de saisie.');
 }
@@ -6,20 +5,6 @@ function alertDoubleProd() {
     alert('Les deux médicaments ne peuvent pas être les mêmes.');
 }
 
-
-//QUAND CHANGE CLICK SUR REMPLACANT
-$(document).ready(function(){
-    $('#remplaDemande').on('click', function(){
-        //SI CHECKED OR NON
-       if (document.getElementById("remplaDemande").checked == true) {
-        $("#remplaDemande").after("<input type='text' name='remplaNom' id='remplaNom'>");
-       }
-       else {
-        $("#remplaNom").remove();  
-       }
-    })
-        
-});
 
 // RANGE COEF RAPPORT FORM
 function showMainTstatValue(newValue)
