c1bee9c2147f11f18a9a9d677e441030ec0bf73b
Houston <willygames56@gmail.com>
2022-05-13 17:28:05 +0200

    redirection du logout apres 5sec

--- logout.php
+++ logout.php
@@ -1,9 +1,18 @@
-Vous êtes déconnecté.<br/>
+Vous êtes déconnecté.<br>
 
-<a href='/'>Cliquez ici pour vous connecter</a>
+<a href='index.php'>Cliquez ici pour vous connecter</a><br>
 
 <?php
 session_start();
 session_destroy();
 session_unset();
 ?>
+
+<script type="text/javascript">
+    //REDIRECTION AUTOMATIQUE
+    function Redirect() {
+        window.location = "index.php";
+    }
+    document.write("Vous allez y être redirigé.");
+    setTimeout('Redirect()', 5000);
+</script>
