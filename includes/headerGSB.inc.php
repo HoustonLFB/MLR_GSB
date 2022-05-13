2564313a3fd99f2a6e3d1c05fc05151fd4d6a688
Houston <willygames56@gmail.com>
2022-05-13 17:27:38 +0200

    creation header GSB

---
+++ includes/headerGSB.inc.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="center">
+        <img id="logo" src="../assets/img/logo-gsb.png">
+    </div>
+</header>
--- visiteur/rapportForm.php
+++ visiteur/rapportForm.php
@@ -24,7 +24,6 @@ $medicamentsAll = $resDBmedicament->fetchAll();
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../assets/css/css.css">
     <script type="text/javascript" language="JavaScript" src="../assets/js/js.js"></script>
-    <script type="text/javascript" language="JavaScript" src="../assets/js/jquery.js"></script>
     <script type="text/javascript" language="JavaScript" src="../assets/js/ajax.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../assets/img/favicon.ico" />
@@ -34,127 +33,128 @@ $medicamentsAll = $resDBmedicament->fetchAll();
 </head>
 
 <body>
-
-    <header>
-        <div class="center">
-            <img id="logo" src="../assets/img/logo-gsb.png">
-        </div>
-    </header>
-    <div class="aligncenter">
+    <?php
+    include("../includes/nav.inc.php");
+    ?>
+    <div class="main">
         <?php
-        // AFFICHAGE DES ERREURS
-        if (isset($_SESSION['erreurDateMsg'])) {
-            echo '<script type="text/javascript">alertMauvaiseDate();</script>';
-            echo "
+        include("../includes/headerGSB.inc.php");
+        ?>
+        <div class="aligncenter">
+            <?php
+            // AFFICHAGE DES ERREURS
+            if (isset($_SESSION['erreurDateMsg'])) {
+                echo '<script type="text/javascript">alertMauvaiseDate();</script>';
+                echo "
                 <div style='color: red;'>
                 La date de visite ne peut pas être ultérieur à la date de saisie.
                 </div>
             ";
-            unset($_SESSION['erreurDateMsg']);
-        }
-        if (isset($_SESSION['erreurDoubleProd'])) {
-            echo '<script type="text/javascript">alertDoubleProd();</script>';
-            echo "
+                unset($_SESSION['erreurDateMsg']);
+            }
+            if (isset($_SESSION['erreurDoubleProd'])) {
+                echo '<script type="text/javascript">alertDoubleProd();</script>';
+                echo "
                 <div style='color: red;'>
                 Les deux médicaments ne peuvent pas être les mêmes.
                 </div>
             ";
-            unset($_SESSION['erreurDoubleProd']);
-        }
-        if (isset($_SESSION['rapAdded'])) {
-            echo '<script type="text/javascript">rapAdded();</script>';
-            echo "
+                unset($_SESSION['erreurDoubleProd']);
+            }
+            if (isset($_SESSION['rapAdded'])) {
+                echo '<script type="text/javascript">rapAdded();</script>';
+                echo "
                 <div style='color: black;'>
                 Le rapport a bien été ajouté.
                 </div>
             ";
-            unset($_SESSION['rapAdded']);
-        }
-        ?>
+                unset($_SESSION['rapAdded']);
+            }
+            ?>
+        </div>
+        <div name="formRapport" class="aligncenter">
+            <form action="ajoutRapport.php" method="POST">
+                <div id="FormGr1">
+                    <div id='Form1' class="aligncenter">
+                        Quel Praticien : <select name="praticienChoix" class="scrollbarthin">
+                            <?php
+                            foreach ($resDBpraticien as $occurencePra) {
+                                echo "<option value='" . $occurencePra['praID'] . "''>" . $occurencePra['praNom'] . " " . $occurencePra['praPrenom'] . "</option>";
+                            }
+                            ?>
+                        </select><br>
+                    </div>
+
+                    <div id='Form2' class="aligncenter">
+                        Remplaçant <input type="checkbox" name="remplaDemande" id="remplaDemande" class="remplaON"><br>
+                    </div>
+                </div>
+
+                <div id="FormGr2">
+                    <div id='Form3' class="aligncenter">
+                        Quand visite ? <input type="date" name="dateVisite" id="dateVisite" required><br>
+                    </div>
+
+                    <div id='Form4' class="aligncenter">
+                        Motif de visite : 
+                        <select name="motif" id="motif" required>
+                            <option value="1">Renouvellement</option>
+                            <option value="2">Nouveauté</option>
+                            <option value="3">Chute de prescriptions</option>
+                            <option value="4">Remplaçant</option>
+                        </select> <br>
+                    </div>
+                </div>
+
+                <div id="FormGr3">
+                    <div id='Form5' class="aligncenter">
+                        Medicament 1 : <select name="medoc1" id="medoc1" class="scrollbarthin" required>
+                            <?php
+                            foreach ($medicamentsAll as $occurenceMedoc) {
+                                $IDmedoc = $occurenceMedoc[0];
+                                $libMedoc = $occurenceMedoc[1];
+                                echo "<option value='$IDmedoc'>$libMedoc</option>";
+                            }
+                            ?>
+                        </select>
+                    </div>
+
+                    <div id='Form6' class="aligncenter">
+                        Medicament 2 : <select name="medoc2" id="medoc2" class="scrollbarthin" required>
+                            <?php
+                            foreach ($medicamentsAll as $occurenceMedoc) {
+                                $IDmedoc = $occurenceMedoc[0];
+                                $libMedoc = $occurenceMedoc[1];
+                                echo "<option value='$IDmedoc'>$libMedoc</option>";
+                            }
+                            ?>
+                        </select><br>
+                    </div>
+                </div>
+
+                <div id='Form7'>
+                    Coefficient de confiance : <br>
+                    <input name="rangeCoef" type="range" style="width: 200px; height: 40px" min="1" max="5" step="1">
+                    <div id="smiles_line">
+                        <div><img src="..\assets\img\smile\Smile1.png" class="smiles"></img></div>
+                        <div><img src="..\assets\img\smile\Smile2.png" class="smiles"></img></div>
+                        <div><img src="..\assets\img\smile\Smile3.png" class="smiles"></img></div>
+                        <div><img src="..\assets\img\smile\Smile4.png" class="smiles"></img></div>
+                        <div><img src="..\assets\img\smile\Smile5.png" class="smiles"></img></div>
+                    </div>
+                </div>
+
+
+                <div id='Form8'>
+                    Bilan du rapport : <br>
+                    <textarea name="rapBilan" id="rapBilan"></textarea><br>
+                </div>
+
+                <input type="submit">
+            </form>
+
+        </div>
     </div>
-    <div name="formRapport" class="aligncenter">
-        <form action="ajoutRapport.php" method="POST">
-            <div id="FormGr1">
-                <div id='Form1' class="aligncenter">
-                    Quel Praticien : <select name="praticienChoix" class="scrollbarthin">
-                        <?php
-                        foreach ($resDBpraticien as $occurencePra) {
-                            echo "<option value='" . $occurencePra['praID'] . "''>" . $occurencePra['praNom'] . " " . $occurencePra['praPrenom'] . "</option>";
-                        }
-                        ?>
-                    </select><br>
-                </div>
-
-                <div id='Form2' class="aligncenter">
-                    Remplaçant <input type="checkbox" name="remplaDemande" id="remplaDemande" class="remplaON"><br>
-                </div>
-            </div>
-
-            <div id="FormGr2">
-                <div id='Form3' class="aligncenter">
-                    Quand visite ? <input type="date" name="dateVisite" id="dateVisite" required><br>
-                </div>
-
-                <div id='Form4' class="aligncenter">
-                    Motif de visite : 
-                    <select name="motif" id="motif" required>
-                        <option value="1">Renouvellement</option>
-                        <option value="2">Nouveauté</option>
-                        <option value="3">Chute de prescriptions</option>
-                        <option value="4">Remplaçant</option>
-                    </select> <br>
-                </div>
-            </div>
-
-            <div id="FormGr3">
-                <div id='Form5' class="aligncenter">
-                    Medicament 1 : <select name="medoc1" id="medoc1" class="scrollbarthin" required>
-                        <?php
-                        foreach ($medicamentsAll as $occurenceMedoc) {
-                            $IDmedoc = $occurenceMedoc[0];
-                            $libMedoc = $occurenceMedoc[1];
-                            echo "<option value='$IDmedoc'>$libMedoc</option>";
-                        }
-                        ?>
-                    </select>
-                </div>
-
-                <div id='Form6' class="aligncenter">
-                    Medicament 2 : <select name="medoc2" id="medoc2" class="scrollbarthin" required>
-                        <?php
-                        foreach ($medicamentsAll as $occurenceMedoc) {
-                            $IDmedoc = $occurenceMedoc[0];
-                            $libMedoc = $occurenceMedoc[1];
-                            echo "<option value='$IDmedoc'>$libMedoc</option>";
-                        }
-                        ?>
-                    </select><br>
-                </div>
-            </div>
-
-            <div id='Form7'>
-                Coefficient de confiance : <br>
-                <input name="rangeCoef" type="range" style="width: 200px; height: 40px" min="1" max="5" step="1">
-                <div id="smiles_line">
-                    <div><img src="..\assets\img\smile\Smile1.png" class="smiles"></img></div>
-                    <div><img src="..\assets\img\smile\Smile2.png" class="smiles"></img></div>
-                    <div><img src="..\assets\img\smile\Smile3.png" class="smiles"></img></div>
-                    <div><img src="..\assets\img\smile\Smile4.png" class="smiles"></img></div>
-                    <div><img src="..\assets\img\smile\Smile5.png" class="smiles"></img></div>
-                </div>
-            </div>
-
-
-            <div id='Form8'>
-                Bilan du rapport : <br>
-                <textarea name="rapBilan" id="rapBilan"></textarea><br>
-            </div>
-
-            <input type="submit">
-        </form>
-
-    </div>
-
 </body>
 
 </html>
