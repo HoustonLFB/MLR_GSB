f08bc6b39067f924a279bc5d68a2ed3864cae381
Houston <willygames56@gmail.com>
2022-05-17 16:35:26 +0200

    remis de jquery

--- visiteur/rapportForm.php
+++ visiteur/rapportForm.php
@@ -24,6 +24,7 @@ $medicamentsAll = $resDBmedicament->fetchAll();
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../assets/css/css.css">
     <script type="text/javascript" language="JavaScript" src="../assets/js/js.js"></script>
+    <script type="text/javascript" language="JavaScript" src="../assets/js/jquery.js"></script>
     <script type="text/javascript" language="JavaScript" src="../assets/js/ajax.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../assets/img/favicon.ico" />
