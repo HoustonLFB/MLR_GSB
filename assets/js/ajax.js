b29c1fc646a4a4c368758c6341b5c9db04402379
Houston <willygames56@gmail.com>
2022-05-17 16:35:06 +0200

    identation

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,12 +1,12 @@
 // AFFICHAGE REMPLACANT BOX
-$(document).ready(function(){
-    $('#remplaDemande').on('click', function(){
+$(document).ready(function () {
+    $('#remplaDemande').on('click', function () {
         //SI CHECKED OR NON
-       if (document.getElementById("remplaDemande").checked == true) {
-        $("#Form2").append("<input type='text' name='remplaNom' id='remplaNom'>");
-       }
-       else {
-        $("#remplaNom").remove();  
-       }
+        if (document.getElementById("remplaDemande").checked == true) {
+            $("#Form2").append("<input type='text' name='remplaNom' id='remplaNom'>");
+        }
+        else {
+            $("#remplaNom").remove();
+        }
     });
 });
